0136a4831a17252a9f504b7ea3f25f5a7fcc17eb
brianswallme <brian.vullers@home.nl>
2019-05-23 11:06:16 +0200

    trailerconnection toevoeg en code verbetering

--- Arduino Trailer/src/main.cpp
+++ Arduino Trailer/src/main.cpp
@@ -1,34 +1,34 @@
 #include <Arduino.h>
-
 #include "Communication.h"
 #include "Trailer.h"
+#include "trailerConnection.h"
+
 void setup() {
    Serial.begin(9600);
    comminucation_bluettooth_start();
+   trailerConnection_setup();
   }
 
 void loop() {
+  trailer_check_message();
+  trailerConnection_Update_Connection();
   if (trailer_get_connection_status() == OK) {
     Serial.println("Connection OK");
     switch (trailer_get_trailer_state()) {
     case OFF:
-      trailer_check_message();
+
       break;
     case SOUND:
-      trailer_check_message();
       trailer_check_distance();
       break;
     case ASSIST:
-      trailer_check_message();
       //trailer_check_distance();
-      //trailer_assist_steering();
+      trailer_assist_steering();
       break;
     }
   }
   else if (trailer_get_connection_status() == NOK) {
     Serial.println("Connection NOK");
-    trailer_check_message();
     trailer_assist_steering();
-
   }
 }
